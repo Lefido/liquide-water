ed51145478ab885645c5b620e9b66b6f6a09d97f
Lefido <lefido@hotmail.com>
2022-09-29 09:55:22 +0200

    Maj des function bulle et verre

--- script.js
+++ script.js
@@ -6,44 +6,11 @@ const liquide = document.querySelector('.liquide')
 var pourcentage;
 var litre;
 
-setInterval(newBall, 50);
-setInterval(moveBall, 10);
 
-function moveBall() {
+setInterval(newBulle, 50);
+setInterval(moveBulle, 10);
 
-    var bulles = document.querySelectorAll('.bulle');
-    bulles.forEach((bulle, num) => {
-
-        let bulleY = bulle.offsetTop;
-        bulleY -= rand(6);
-        bulle.style.top = bulleY + "px";
-
-        if (bulleY < 0) {
-            bulle.remove();
-        }
-        bulle.style.top = bulleY + "px";
-    })
-
-}
-
-function newBall() {
-
-    let bulle = document.createElement('div');
-    bulle.classList.add('bulle');
-    bulle.style.top = (liquide.offsetHeight) + "px"
-    bulle.style.left = rand(liquide.offsetWidth) + "px"
-    
-    bulle.style.top += "px"
-    bulle.style.left += "px"
-
-    let tailleBulle = rand(5)
-    bulle.style.width = tailleBulle + "px"
-    bulle.style.height = tailleBulle + "px"
-
-    liquide.appendChild(bulle);
-
-}
-
+// Selection des petit contenant
 verres.forEach((verre, num) => {
     verre.addEventListener('click', function() {
 
@@ -55,11 +22,12 @@ verres.forEach((verre, num) => {
             pourcentage += 12.5
             litre -= 0.250
         }
-        actif(num);
+        activeVerre(num);
     })
 })
 
-function actif(valeur){
+// Active les verres sélectionnée
+function activeVerre(valeur){
 
     verres.forEach((verre, num) => {
         
@@ -79,11 +47,45 @@ function actif(valeur){
     })
 }
 
+// Animation des bulles
+function moveBulle() {
+
+    var bulles = document.querySelectorAll('.bulle');
+    bulles.forEach((bulle, num) => {
+
+        let bulleY = bulle.offsetTop;
+        bulleY -= rand(6);
+        bulle.style.top = bulleY + "px";
+
+        if (bulleY < 0) {
+            bulle.remove();
+        }
+        bulle.style.top = bulleY + "px";
+    })
+
+}
+
+// Ajout de nouvelles bulles
+function newBulle() {
+
+    let bulle = document.createElement('div');
+    bulle.classList.add('bulle');
+    bulle.style.top = (liquide.offsetHeight) + "px"
+    bulle.style.left = rand(liquide.offsetWidth) + "px"
+    
+    bulle.style.top += "px"
+    bulle.style.left += "px"
+
+    let tailleBulle = rand(5)
+    bulle.style.width = tailleBulle + "px"
+    bulle.style.height = tailleBulle + "px"
+
+    liquide.appendChild(bulle);
+
+}
+
+// Fonction randomise avec paramètre
 function rand(valeur) {
     return 1 + Math.floor(Math.random() * valeur) 
 }
-
-
-
-
 
